9cc88d962342da3e70cff1cdbb2160abc9edeec0
tinolebat <jsdah07@gmail.com>
2020-05-18 23:18:45 +0800

    create the routes for each data model

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 27017;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -15,6 +15,12 @@ mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true}).catch(err =
 const connection = mongoose.connection;
 connection.once('open', () => { console.log("MongoDB database connection established successfully") 
 });
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, ()=> {
     console.log(`Server is running on port: ${port}`);
